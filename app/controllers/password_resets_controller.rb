7e40aaa6effb58facbaf46efc273371dd11d2541
Alvin Jing <jingqi.hanzhong@gmail.com>
2015-07-16 18:39:35 -0300

    Edited update action in password_resets_controller

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -17,7 +17,7 @@ class PasswordResetsController < ApplicationController
 	def edit
 		@user = User.find_by(password_reset_token: params[:id])
 		if @user
-		
+			
 		else
 			render file: "public/404.html", status: :not_found
 		end
@@ -25,10 +25,19 @@ class PasswordResetsController < ApplicationController
 
 	def update
 		user = User.find_by(password_reset_token: params[:id])
-		if user
-			
+		if user && user.update_attributes(user_params)
+			user.update_attribute(:password_reset_token, nil)
+			flash.now[:notice] = "Password updated"
+			session[:user_id] = user.id
+			redirect_to todo_lists_path
 		else
-
+			flash.now[:notice] = "Password reset token not found"
+			render action: :edit
 		end
+	end
+
+	private 
+	def user_params
+		params.require(:user).permit(:password, :password_confirmation)
 	end
 end
--- spec/controllers/password_resets_controller_spec.rb
+++ spec/controllers/password_resets_controller_spec.rb
@@ -16,7 +16,33 @@ describe PasswordResetsController do
 
 		it "generates new password_reset_token for the user" do
 			expect(User).to receive(:find_by).with(email: user.email).and_return(user)
-			expect{ post :create, email: user.email }.to change{user.password_reset_tokenraek}
+			expect{ post :create, email: user.email }.to change{user.password_reset_token}
+		end
+	end
+
+	context "PATCH update" do
+
+		let(:user){ create(:user) }
+		before do
+			user.generate_password_reset_token! 
+		end
+
+		it "sets the user password_reset_token to empty" do
+			patch :update, id: user.password_reset_token, user: { password: "alvinjing", 
+				password_confirmation: "alvinjing" }
+			user.reload
+			expect(flash[:notice]).to match(/Password updated/i)
+			expect(user.password_reset_token).to be_blank
+		end
+
+		it "logs the user in by setting the user session id" do 
+			patch :update, id: user.password_reset_token, user: { password: "alvinjing", 
+				password_confirmation: "alvinjing" }
+			user.reload
+		end
+
+		it "flash password and password_confirmation not the same" do 
+
 		end
 	end
 end
